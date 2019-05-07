70fe6fd25888db1bc92cd9c6bac9ac50d7fde76a
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2019-05-07 14:40:35 +0200

    Fixed linting problems

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const commander = require('commander');
 const { blue, green, red } = require('kleur');
 const childProcess = require('child_process');
@@ -6,81 +7,79 @@ const packageJson = require('./package.json');
 
 commander.version(packageJson.version)
   .option('-E, --exact', 'install exact version')
-  .option('-T, --tilde', 'install most recent version with the same minor version')
+  .option('-T, --tilde', 'install most recent version with the same minor version');
 commander.on('--help', () => {
   console.log('');
-  console.log('When no options are given, packages are installed with a caret (^).')
+  console.log('When no options are given, packages are installed with a caret (^).');
 });
 commander.parse(process.argv);
 
 if (commander.exact && commander.tilde) {
-  console.log(`Exact and tilde options can't be on, on the same time.`);
+  console.log('Exact and tilde options can\'t be on, on the same time.');
   process.exit(1);
 }
 
 const logError = (message) => {
   console.log(red('[Error]'), `: ${message}`);
-}
+};
 
 const logInfo = (message) => {
   console.log(blue('[Start]'), `: ${message}`);
-}
+};
 
 const logSuccess = (message) => {
   console.log(green('[Done]'), `: ${message}`);
-}
-
-const addYarnOptions = (command) => {
-  command = addExact(command);
-  command = addTilde(command);
-  return command;
-}
+};
 
 const addExact = (command) => {
   if (commander.exact) {
-    return command + ` --exact`;
-  } else {
-    return command;
+    return `${command} --exact`;
   }
-}
+  return command;
+};
 
 const addTilde = (command) => {
   if (commander.tilde) {
-     return command + ` --tilde`;
-  } else {
-    return command;
+    return `${command} --tilde`;
   }
-}
-
-const options = {
-  devDependencies: ' --dev',
-  peerDependencies: ' --peer'
+  return command;
 };
 
-let tempCommand = '';
+const addYarnOptions = (command) => {
+  let tempCommand = addExact(command);
+  tempCommand = addTilde(command);
+  return tempCommand;
+};
+
+const options = {
+  devDependencies: '--dev',
+  peerDependencies: '--peer',
+};
+
 let command = '';
-for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
+const elements = ['dependencies', 'devDependencies', 'peerDependencies'];
+elements.forEach((element) => {
   if (packageJson[element]) {
+    let tempCommand;
     const option = options[element];
     const packages = Object.keys(packageJson[element]);
     let packageList = '';
 
-    for (let pkg of packages) {
+    packages.forEach((pkg) => {
       if (packageList.length) {
         packageList += ` ${pkg}`;
       } else {
         packageList = pkg;
       }
-    }
+    });
 
     if (element === 'dependencies') {
       tempCommand = `yarn add ${packageList}`;
-      tempCommand = addYarnOptions(tempCommand);
     }
-    if (element === 'devDependencies' || element == 'peerDependencies') {
+    if (element === 'devDependencies' || element === 'peerDependencies') {
       tempCommand = `yarn add ${packageList} ${option}`;
-      tempCommand = addYarnOptions(tempCommand);
     }
+    tempCommand = addYarnOptions(tempCommand);
 
     if (command.length) {
       command += ` && ${tempCommand}`;
@@ -88,12 +87,12 @@ for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
       command = tempCommand;
     }
   }
-}
+});
 
 try {
   logInfo(command);
   childProcess.execSync(command, {
-    stdio: []
+    stdio: [],
   });
   logSuccess(command);
 } catch (e) {
