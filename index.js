06d0bf3facf0735ad0e5a50e87a3b87e50243c14
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2019-05-07 10:41:25 +0200

    Initial version of the application

---
+++ index.js
@@ -0,0 +1,98 @@
+const commander = require('commander');
+const childProcess = require('child_process');
+
+const packageJson = require('./package.json');
+
+commander.version(packageJson.version)
+  .option('-E, --exact', 'Install exact version')
+  .option('-T, --tilde', 'Install most recent version with the same minor version')
+  .parse(process.argv);
+
+const logError = (message) => {
+  console.log(`[Error]: ${message}`);
+}
+
+const logInfo = (message) => {
+  console.log(`[Start]: ${message}`);
+}
+
+const logSuccess = (message) => {
+  console.log(`[Done]: ${message}`);
+}
+
+const options = {
+  dependencies: '',
+  devDependencies: ' --dev',
+  peerDependencies: ' --peer'
+};
+
+let dependencyCommand = ''
+let devDependencyCommand = ''
+let peerDependencyCommand = '';
+let command = '';
+for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
+  if (packageJson[element]) {
+    const option = options[element];
+    const packages = Object.keys(packageJson[element]);
+
+
+    for (let pkg of packages) {
+      if (element === 'dependencies') {
+        dependencyCommand = `yarn add ${pkg}`;
+        if (commander.exact) {
+          dependencyCommand += ` --exact`;
+        }
+        if (commander.tilde) {
+          dependencyCommand += ` --tilde`
+        }
+      }
+      if (element === 'devDependencies') {
+        devDependencyCommand = `yarn add ${pkg} --dev`;
+        if (commander.exact) {
+          devDependencyCommand += ` --exact`;
+        }
+        if (commander.tilde) {
+          devDependencyCommand += ` --tilde`
+        }
+      }
+      if (element === 'peerDependencies') {
+        peerDependencyCommand = `yarn add ${pkg} --peer`;
+        if (commander.exact) {
+          peerDependencyCommand += ` --exact`;
+        }
+        if (commander.tilde) {
+          peerDependencyCommand += ` --tilde`
+        }
+      }
+    }
+  }
+}
+
+if (dependencyCommand.length) {
+  command += dependencyCommand;
+}
+if (devDependencyCommand.length) {
+  if (command.length) {
+    command += ` && ${devDependencyCommand}`;
+  } else {
+    command += devDependencyCommand;
+  }
+}
+if (peerDependencyCommand.length) {
+  if (command.length) {
+    command += ` && ${peerDependencyCommand}`;
+  } else {
+    command += peerDependencyCommand;
+  }
+}
+console.log(command);
+
+try {
+  logInfo(command);
+  childProcess.execSync(command, {
+    stdio: []
+  });
+  logSuccess(command);
+} catch (e) {
+  logError(`${command} - ${e}`);
+}
